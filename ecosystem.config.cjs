387541476c7c5a7fe01bac780ae730d21bc99980
Samuelba._. <hello@tutorapide.xyz>
2026-07-08 01:09:02 +0200

    Create ecosystem.config.cjs

---
+++ ecosystem.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'votehub-dashboard',
+      script: 'server/index.js',
+      cwd: __dirname,
+      instances: 1,
+      exec_mode: 'fork',
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
+        PORT: 4000,
+      },
+    },
+  ],
+};
